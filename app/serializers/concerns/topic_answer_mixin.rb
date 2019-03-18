d0cf9c258b81b781412f897d4a7d219039411814
Vinoth Kannan <vinothkannan@vinkas.com>
2019-03-18 18:02:55 +0530

    FIX: Include the serializer fields only if plugin is enabled

--- app/serializers/concerns/topic_answer_mixin.rb
+++ app/serializers/concerns/topic_answer_mixin.rb
@@ -7,6 +7,10 @@ module TopicAnswerMixin
     object.custom_fields["accepted_answer_post_id"] ? true : false
   end
 
+  def include_has_accepted_answer?
+    SiteSetting.solved_enabled
+  end
+
   def can_have_answer
     return true if SiteSetting.allow_solved_on_all_topics
     return false if object.closed || object.archived
@@ -14,6 +18,6 @@ module TopicAnswerMixin
   end
 
   def include_can_have_answer?
-    SiteSetting.empty_box_on_unsolved
+    SiteSetting.solved_enabled && SiteSetting.empty_box_on_unsolved
   end
 end
