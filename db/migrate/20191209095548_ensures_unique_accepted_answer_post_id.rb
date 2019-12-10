b6330a3b6bce397e61f957d6229e70e8658f9801
jjaffeux <j.jaffeux@gmail.com>
2019-12-10 11:13:01 +0100

    more readable pattern

--- db/migrate/20191209095548_ensures_unique_accepted_answer_post_id.rb
+++ db/migrate/20191209095548_ensures_unique_accepted_answer_post_id.rb
@@ -3,11 +3,13 @@
 class EnsuresUniqueAcceptedAnswerPostId < ActiveRecord::Migration[5.2]
   def change
     execute <<~SQL
-      DELETE FROM topic_custom_fields f
-      WHERE name = 'accepted_answer_post_id' AND id > (
-        SELECT MIN(f2.id) FROM topic_custom_fields f2
-          WHERE f2.topic_id = f.topic_id AND f2.name = f.name
-      )
+      DELETE FROM topic_custom_fields AS tcf1
+      USING topic_custom_fields AS tcf2
+      WHERE tcf1.id > tcf2.id
+         AND tcf1.value = tcf2.value
+         AND tcf1.name = 'accepted_answer_post_id'
+         AND tcf2.name = 'accepted_answer_post_id'
+      SQL
     SQL
 
     add_index :topic_custom_fields,
