072a8e8c3d72572e548d0bbd723834cad06cae5d
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2019-12-12 10:26:43 +0100

    Update 20191209095548_ensures_unique_accepted_answer_post_id.rb

--- db/migrate/20191209095548_ensures_unique_accepted_answer_post_id.rb
+++ db/migrate/20191209095548_ensures_unique_accepted_answer_post_id.rb
@@ -6,9 +6,8 @@ class EnsuresUniqueAcceptedAnswerPostId < ActiveRecord::Migration[5.2]
       DELETE FROM topic_custom_fields AS tcf1
       USING topic_custom_fields AS tcf2
       WHERE tcf1.id > tcf2.id
-         AND tcf1.value = tcf2.value
+         AND tcf1.name = tcf2.name
          AND tcf1.name = 'accepted_answer_post_id'
-         AND tcf2.name = 'accepted_answer_post_id'
       SQL
     SQL
 
