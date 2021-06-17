7351882171b835421bfebb42528807b00ff4c054
Mark VanLandingham <mark.vanlandingham@discourse.org>
2021-06-17 12:37:29 -0500

    Revert "Use `add_directory_column` API to add solutions count to user directory (#137)" (#139)
    This reverts commit 58c594fdda0a8c35a6f52079e1d8909619f6e2df.

--- db/migrate/20210609171830_add_solutions_to_directory_items.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddSolutionsToDirectoryItems < ActiveRecord::Migration[6.1]
-  def change
-    add_column :directory_items, :solutions, :integer, default: 0
-  end
-end
