4afcd4568942e3d89a43f627f95732850d408924
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-08-15 14:23:31 +1000

    FIX: granular event

--- spec/fabricators/solved_hook_fabricator.rb
+++ spec/fabricators/solved_hook_fabricator.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Fabricator(:solved_web_hook, from: :web_hook) do
-  transient solved_hook: WebHookEventType.find_by(name: "solved")
+  transient solved_hook: WebHookEventType.find_by(name: "accepted_solution"),
+            unsolved_hook: WebHookEventType.find_by(name: "unaccepted_solution")
 
-  after_build { |web_hook, transients| web_hook.web_hook_event_types = [transients[:solved_hook]] }
+  after_build do |web_hook, transients|
+    web_hook.web_hook_event_types = [transients[:solved_hook], transients[:unsolved_hook]]
+  end
 end
