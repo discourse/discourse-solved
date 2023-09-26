c81585fba0aede20bab22105a3ff0bc171d79633
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-09-26 11:36:11 +1000

    FIX: update after changes

--- spec/fabricators/solved_hook_fabricator.rb
+++ spec/fabricators/solved_hook_fabricator.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Fabricator(:solved_web_hook, from: :web_hook) do
-  transient solved_hook: WebHookEventType.find_by(name: "accepted_solution"),
-            unsolved_hook: WebHookEventType.find_by(name: "unaccepted_solution")
-
-  after_build do |web_hook, transients|
-    web_hook.web_hook_event_types = [transients[:solved_hook], transients[:unsolved_hook]]
+  after_build do |web_hook|
+    web_hook.web_hook_event_types =
+      WebHookEventType.where(name: %w[accepted_solution unaccepted_solution])
   end
 end
