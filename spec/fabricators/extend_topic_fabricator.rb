1a07c87e7189f0b5e6197cca378a015c160496c6
Linca <41134017+Lhcfl@users.noreply.github.com>
2024-09-10 14:26:11 +0800

    Update spec/fabricators/extend_topic_fabricator.rb
    Co-authored-by: Bianca Nenciu <nbianca@users.noreply.github.com>

--- spec/fabricators/extend_topic_fabricator.rb
+++ spec/fabricators/extend_topic_fabricator.rb
@@ -3,7 +3,7 @@ Fabricator(:custom_topic, from: :topic) do
   transient :custom_topic_name
   transient :value
   after_create do |top, transients|
-    if (transients[:custom_topic_name] == DiscourseSolved::ACCEPTED_ANSWER_POST_ID_CUSTOM_FIELD)
+    if transients[:custom_topic_name] == DiscourseSolved::ACCEPTED_ANSWER_POST_ID_CUSTOM_FIELD
       post = Fabricate(:post)
       Fabricate(:solution, topic_id: top.id, answer_post_id: post.id)
     end
