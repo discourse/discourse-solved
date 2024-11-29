de5f7cb56d3430b8611eae2dc8f76448f6f1b666
David Taylor <david@taylorhq.com>
2024-11-29 13:15:19 +0000

    DEV: Convert to native class syntax

--- assets/javascripts/discourse/routes/user-activity-solved.js
+++ assets/javascripts/discourse/routes/user-activity-solved.js
@@ -1,9 +1,9 @@
 import UserActivityStreamRoute from "discourse/routes/user-activity-stream";
 import I18n from "I18n";
 
-export default UserActivityStreamRoute.extend({
-  userActionType: 15,
-  noContentHelpKey: "solved.no_solutions",
+export default class UserActivitySolved extends UserActivityStreamRoute {
+  userActionType = 15;
+  noContentHelpKey = "solved.no_solutions";
 
   emptyState() {
     const user = this.modelFor("user");
@@ -20,5 +20,5 @@ export default UserActivityStreamRoute.extend({
     }
 
     return { title, body };
-  },
-});
+  }
+}
