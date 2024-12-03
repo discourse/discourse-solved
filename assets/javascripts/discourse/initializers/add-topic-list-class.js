024d9dd70f5a2510fd1744aa3108fda44cf0201e
Jarek Radosz <jradosz@gmail.com>
2024-12-03 15:09:43 +0100

    DEV: Use `topic-list-item-class` transformer (#321)
    …to replace a deprecated topic-list-item customization

--- assets/javascripts/discourse/initializers/add-topic-list-class.js
+++ assets/javascripts/discourse/initializers/add-topic-list-class.js
@@ -1,18 +1,36 @@
 import TopicListItem from "discourse/components/topic-list-item";
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default {
   name: "add-topic-list-class",
+
   initialize() {
-    TopicListItem.reopen({
-      @discourseComputed()
-      unboundClassNames() {
-        let classList = this._super(...arguments);
-        if (this.topic.has_accepted_answer) {
-          classList += " status-solved";
+    withPluginApi("1.39.0", (api) => {
+      // TODO: cvx - remove after the glimmer topic list transition
+      withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
+        TopicListItem.reopen({
+          @discourseComputed()
+          unboundClassNames() {
+            let classList = this._super(...arguments);
+            if (this.topic.has_accepted_answer) {
+              classList += " status-solved";
+            }
+            return classList;
+          },
+        });
+      });
+
+      api.registerValueTransformer(
+        "topic-list-item-class",
+        ({ value, context }) => {
+          if (context.topic.get("has_accepted_answer")) {
+            value.push("status-solved");
+          }
+          return value;
         }
-        return classList;
-      },
+      );
     });
   },
 };
