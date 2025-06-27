3c103b843c0e863ffdd248f43c677c0e53aaa4cf
Nat <natalie.tay@discourse.org>
2025-06-27 11:31:29 +0800

    controller test

--- app/controllers/discourse_solved/solved_topics_controller.rb
+++ app/controllers/discourse_solved/solved_topics_controller.rb
@@ -4,7 +4,7 @@ class DiscourseSolved::SolvedTopicsController < ::ApplicationController
   requires_plugin DiscourseSolved::PLUGIN_NAME
 
   def by_user
-    params.permit(:username)
+    params.require(:username)
     user =
       fetch_user_from_params(
         include_inactive:
@@ -22,8 +22,13 @@ class DiscourseSolved::SolvedTopicsController < ::ApplicationController
           "INNER JOIN discourse_solved_solved_topics ON discourse_solved_solved_topics.answer_post_id = posts.id",
         )
         .joins(:topic)
+        .joins("LEFT JOIN categories ON categories.id = topics.category_id")
         .where(user_id: user.id, deleted_at: nil)
         .where(topics: { archetype: Archetype.default, deleted_at: nil })
+        .where(
+          "topics.category_id IS NULL OR NOT categories.read_restricted OR topics.category_id IN (:secure_category_ids)",
+          secure_category_ids: guardian.secure_category_ids,
+        )
         .includes(:user, topic: %i[category tags])
         .order("discourse_solved_solved_topics.created_at DESC")
         .offset(offset)
